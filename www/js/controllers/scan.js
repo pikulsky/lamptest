30c5509a866675109c0089f111712666f28c86aa
Ilya Kurnosenkov <ilya.kurnosenkov@arcadia.spb.ru>
2015-12-09 21:03:23 +0300

    Debug scanning.

--- www/js/controllers/scan.js
+++ www/js/controllers/scan.js
@@ -3,6 +3,19 @@ function ScanCtrl() {
 
   self.scanBarcode = function() {
     alert('Scan!');
+
+    cordova.plugins.barcodeScanner.scan(
+      function (result) {
+        alert("We got a barcode\n" +
+          "Result: " + result.text + "\n" +
+          "Format: " + result.format + "\n" +
+          "Cancelled: " + result.cancelled);
+      },
+      function (error) {
+        alert("Scanning failed: " + error);
+      }
+    );
+
     /*$cordovaBarcodeScanner.scan().then(function(imageData) {
       alert(imageData.text);
       console.log("Barcode Format -> " + imageData.format);
