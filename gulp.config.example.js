a0f498df2ffa94b7e45f80100c7c2363b4f38ca7
Ilya Kurnosenkov <ilya.kurnosenkov@arcadia.spb.ru>
2015-12-21 16:41:46 +0300

    gulp-hockeyapp

--- gulp.config.example.js
+++ gulp.config.example.js
@@ -1,5 +1,20 @@
 module.exports = {
-  someConfig: {
-    test: 'value'
+  hockeyApp: {
+    platforms: {
+      android: {
+        id: '123123',
+        file: 'android.apk'
+      },
+      ios: {
+        id: '456456',
+        file: 'ios.ipa'
+      },
+      wp: {
+        id: '789789',
+        file: 'wp8.xap'
+      }
+    },
+    apiToken: 'secretapitoken',
+    buildTeams: [1, 2, 3]
   }
 };
