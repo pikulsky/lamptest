dc591a8d8b77e32db68e3b16e88fc7d6103b1203
Ilya Kurnosenkov <ilya.kurnosenkov@arcadia.spb.ru>
2015-12-23 16:49:36 +0300

    Set up gulp build task.

--- README.md
+++ README.md
@@ -4,3 +4,13 @@ LampTest
 A PhoneGap-based mobile application for scanning lamp barcode and getting useful information about that particular product, e.g. real measured luminosity, power consumption, etc.
 
 It uses [LampTest](http://lamptest.ru) site as a data source.
+
+Build
+-----
+
+```
+npm install
+gulp
+```
+
+Application building process depends on build.phonegap.com service, so existing build.phonegap.com service account is required.
--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,49 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
-var bower = require('bower');
-var concat = require('gulp-concat');
-var sass = require('gulp-sass');
-var rename = require('gulp-rename');
+var phonegapBuild = require('gulp-phonegap-build');
+var del = require('del');
+var merge = require('merge-stream');
+var process = require('process');
 
-gulp.task('default', []);
+var config = require('./gulp.config.local.js');
+
+gulp.task('default', ['phonegap-build-android']);
+
+gulp.task('clean', function(done) {
+  del.sync([
+    'build/*'
+  ]);
+  done();
+});
+
+gulp.task('assemble-assets', ['clean'], function() {
+  var configCopy = gulp.src('config.xml')
+    .pipe(gulp.dest('build/www'));
+
+  var srcCopy = gulp.src('www/**/*')
+    .pipe(gulp.dest('build/www'));
+
+  var resourcesCopy = gulp.src('resources/**/*')
+    .pipe(gulp.dest('build/www/resources'));
+
+  return merge(configCopy, srcCopy, resourcesCopy);
+});
+
+gulp.task('phonegap-build-android', ['assemble-assets'], function(done) {
+  gulp.src('build/www/**/*')
+    .pipe(phonegapBuild({
+      appId: config.phonegapBuild.appId,
+      user: config.phonegapBuild.user,
+      keys: config.phonegapBuild.keys,
+      download: {
+        android: 'build/android.apk'
+      }
+    }))
+    .on('pg-error', function(err) {
+      gutil.log(err);
+      process.exit(1);
+    })
+    .on('pg-sent', function() {
+      done();
+    });
+});
