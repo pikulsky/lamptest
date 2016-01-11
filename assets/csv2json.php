1ba6fd083c65f63878f01d2cb26e52eecf6e4b1b
Ilya Kurnosenkov <ilya.kurnosenkov@arcadia.spb.ru>
2016-01-11 16:50:00 +0300

    Download fresh data from lamptest site. Convert to json using UPCs as keys.

--- assets/csv2json.php
+++ assets/csv2json.php
@@ -17,31 +17,33 @@ $data = fgetcsv($handle, 0, ';');
 // Convert CSV data to JSON
 while (($data = fgetcsv($handle, 0, ';')) !== FALSE) {
   $row++;
-	  
-  $json[$data[0]] = [
-    'id' => $data[0],
-    'brand' => $data[1],
-    'model' => $data[2],
-    'P' => $data[3],
-    'link' => $data[4],
-    'prop5' => $data[5],
-    'price_rur' => $data[6],
-    'price_usd' => $data[7],
-    'upc' => $data[8],
-    'diameter' => $data[9],
-    'height' => $data[10],
-    'voltage' => $data[11],
-    'base_type' => $data[12],
-    'class' => $data[13],
-    'type' => $data[14],
-    'subtype' => $data[15],
-    'matte' => $data[16],
-    'lm' => $data[17],
-    'ekv' => $data[18],
-    'color' => $data[19],
-    'age' => $data[20],
-    'p' => $data[21],
-  ];
+
+  if ($data[8]) {
+    $json[$data[8]] = [
+        'id' => $data[0],
+        'brand' => $data[1],
+        'model' => $data[2],
+        'P' => $data[3],
+        'link' => $data[4],
+        'prop5' => $data[5],
+        'price_rur' => $data[6],
+        'price_usd' => $data[7],
+        'upc' => $data[8],
+        'diameter' => $data[9],
+        'height' => $data[10],
+        'voltage' => $data[11],
+        'base_type' => $data[12],
+        'class' => $data[13],
+        'type' => $data[14],
+        'subtype' => $data[15],
+        'matte' => $data[16],
+        'lm' => $data[17],
+        'ekv' => $data[18],
+        'color' => $data[19],
+        'age' => $data[20],
+        'p' => $data[21],
+    ];
+  }
 }
 
 fclose($handle);
