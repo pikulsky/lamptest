8bba77435dc9f466e4bc1d9b63541703dc981bad
Ilya Kurnosenkov <ilya.kurnosenkov@nantmobile.com>
2016-02-29 00:15:27 +0300

    Start local dev server as gulp task instead of ionic.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
 var gulp = require('gulp');
+var livereload = require('gulp-livereload');
 var gutil = require('gulp-util');
+var sequence = require('gulp-sequence');
 var phonegapBuild = require('gulp-phonegap-build');
 var del = require('del');
 var merge = require('merge-stream');
 var process = require('process');
+var express = require('express');
+var connectLr = require('connect-livereload');
 
 var config = require('./gulp.config.local.js');
 
@@ -27,6 +31,35 @@ gulp.task('assemble-assets', ['clean'], function() {
     .pipe(gulp.dest('build/www/resources'));
 
   return merge(configCopy, srcCopy, resourcesCopy);
+});
+
+// Start local express server
+gulp.task('serve', ['assemble-assets'], function () {
+  express()
+    .use(connectLr())
+    .use(express.static('build/www'))
+    .get('/cordova.js', function (req, res) {
+      return res.status(200).send('// mocked cordova.js response to prevent 404 errors during development')
+    })
+    .listen(8100)
+});
+
+gulp.task('watch', ['serve'], function () {
+  livereload.listen();
+  return gulp.watch([
+    'www/index.html'
+    , 'www/css/**/*'
+    , 'www/js/**/*'
+    , 'www/templates/**/*'
+  ], ['compile-reload']);
+});
+
+gulp.task('compile-reload', function() {
+  return sequence('assemble-assets', 'reload')();
+});
+
+gulp.task('reload', function () {
+  livereload.changed('project');
 });
 
 gulp.task('phonegap-build-android', ['assemble-assets'], function(done) {
