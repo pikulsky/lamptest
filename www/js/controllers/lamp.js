1a680c45db858dbc659d86492f26847793822826
Ilya Kurnosenkov <ilya.kurnosenkov@arcadia.spb.ru>
2016-08-31 14:56:30 +0300

    Fixed issue with incorrect generation of urls for certain lamps.

--- www/js/controllers/lamp.js
+++ www/js/controllers/lamp.js
@@ -4,8 +4,8 @@ function LampCtrl($stateParams) {
   if ($stateParams.upc && window.data && window.data[$stateParams.upc]) {
     var lamp = window.data[$stateParams.upc];
 
-    var normalizedBrand = lamp.brand.toLowerCase().replace(/\s+/g, '-').replace(/[^A-Za-z0-9\-]/g, '');
-    var normalizedModel = lamp.model.toLowerCase().replace(/\s+/g, '-').replace(/[^A-Za-z0-9\-]/g, '');
+    var normalizedBrand = lamp.brand.toLowerCase().replace(/[\s\/]+/g, '-').replace(/[^A-Za-z0-9\-]/g, '');
+    var normalizedModel = lamp.model.toLowerCase().replace(/[\s\/]+/g, '-').replace(/[^A-Za-z0-9\-]/g, '');
 
     self.pageLink = 'http://lamptest.ru/review/' + normalizedBrand + '-' + normalizedModel;
     self.lampPhoto = 'http://lamptest.ru/images/photo/' + normalizedBrand + '-' + normalizedModel + '.jpg';
