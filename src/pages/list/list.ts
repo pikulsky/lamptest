034a104ace40f0ba06e4ffd552ebae861bc2b19c
Ilya Kurnosenkov <ilya.kurnosenkov@arcadia.spb.ru>
2017-01-25 17:02:08 +0300

    Basic lamps list.

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -2,14 +2,33 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
+declare var data: any;
+
 @Component({
   selector: 'page-list',
   templateUrl: 'list.html'
 })
 export class ListPage {
+  lamps: any[] = [];
 
   constructor(public navCtrl: NavController) {
 
+    for (let upc in data) {
+      if (data.hasOwnProperty(upc)) {
+        let title = data[upc].brand + ' ' + data[upc].model;
+        if (title) {
+          this.lamps.push({
+            title: title,
+            upc: upc,
+            normalizedTitle: title.toLowerCase()
+          });
+        }
+      }
+    }
+  }
+
+  lampClicked(upc: string) {
+    console.log(upc);
   }
 
 }
