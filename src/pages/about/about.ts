99494fa90dc79ad65bb9bd687c7c69a1e4165bd4
Ilya Kurnosenkov <ilya.kurnosenkov@arcadia.spb.ru>
2017-01-25 14:39:07 +0300

    Added latest update date to about page.

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -2,14 +2,33 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
+declare var data: any;
+declare var moment: any;
+
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
+  totalLamps: number;
+  latestUpdate: any;
 
+  constructor(public navCtrl: NavController) {
+    this.totalLamps = 0;
+    this.latestUpdate = moment('1970-01-01');
+
+    for (var upc in data) {
+      if (data.hasOwnProperty(upc)) {
+        this.totalLamps++;
+        let testDate = moment(data[upc].test_date, 'DD.MM.YYYY');
+        if (testDate > this.latestUpdate) {
+          this.latestUpdate = testDate;
+        }
+      }
+    }
+
+    this.latestUpdate = this.latestUpdate.format('Do MMMM, YYYY');
   }
 
 }
