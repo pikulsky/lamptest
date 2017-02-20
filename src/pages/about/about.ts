083c405e1c7ac5fbffb3d8674dc1ea7a57991a04
Ilya Kurnosenkov <ilya.kurnosenkov@arcadia.spb.ru>
2017-02-20 14:41:16 +0300

    Fixed issue with broken DB stats on About page.

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
-declare var data: any;
-declare var moment: any;
+declare let data: any;
+declare let moment: any;
 
 @Component({
   selector: 'page-about',
@@ -18,12 +18,14 @@ export class AboutPage {
     this.totalLamps = 0;
     this.latestUpdate = moment('1970-01-01');
 
-    for (var upc in data) {
+    for (let upc in data) {
       if (data.hasOwnProperty(upc)) {
-        this.totalLamps++;
-        let testDate = moment(data[upc].test_date, 'DD.MM.YYYY');
-        if (testDate > this.latestUpdate) {
-          this.latestUpdate = testDate;
+        for (let idx = 0; idx < data[upc].length; idx++) {
+          this.totalLamps++;
+          let testDate = moment(data[upc][idx].test_date, 'DD.MM.YYYY');
+          if (testDate > this.latestUpdate) {
+            this.latestUpdate = testDate;
+          }
         }
       }
     }
